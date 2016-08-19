6bf963e7af09b1574df190d449e8de7b0a52a636
Adilson Alves <adilson.day@hotmail.com>
2016-08-19 08:58:53 -0300

    Tela de login
    Com arquivos do bootstrap e etc

---
+++ view/header.php
@@ -0,0 +1,28 @@
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+      
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+              data-target="#btn-collapse-nav" aria-expanded="false">
+                
+                <span class="sr-only">Recolher barra de navegação</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.php">Shyme</a>
+        </div>  
+        
+        <div class="collapse navbar-collapse" id="btn-collapse-nav">
+            <ul class="nav navbar-nav">
+                  <form class="navbar-form navbar-left" role="search">
+                      <div class="form-group">
+                          <input class="form-control" type="text" name="search" placeholder"Procurar" />
+                            
+                      </div>
+                      <button type="submit" class="btn btn-default">Buscar</button>
+                  </form>
+            </ul>
+        </div>
+    </div>
+</nav>
---
+++ view/login.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Login - Shyme</title>
+        <link rel="stylesheet" href="css/bootstrap.css" />
+        <link rel="stylesheet" href="css/login.css" />
+    </head>    
+    
+    <body>
+        <nav class="navbar navbar-default">
+            <a class="navbar-brand" href="index.php">Shyme</a>
+        </nav>
+        
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12 wrapper">
+                    <h2>Bem-vindo ao Shyme</h2>
+                    <form action="#" method="POST">
+                        <!-- Formulario -->
+                        <fieldset>        
+                            <div class="form-group has-warning">
+                                <label class="sr-only" for="txt_login">Login</label>
+                                <input type="text" id="txt_login" name="txt_login" placeholder="Login" 
+                                  pattern=".{9,}"/>
+                            </div>
+                            
+                            <div class="form-group has-warning">
+                                <label class="sr-only" for="txt_senha">Senha</label>
+                                <input type="password" id="txt_senha" name="txt_senha" placeholder="Senha" />
+                            </div>
+                            
+                            <button class="btn btn-primary">Logar</button>
+                        </fieldset>
+                       
+                    </form> <!-- Formulario -->
+                </div> <!-- Coluna -->
+            </div> <!-- row -->
+        </div> <!-- container -->
+        
+        <script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.js"></script>
+        
+    </body>
+</html>
